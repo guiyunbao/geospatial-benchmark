571d129f439c976a7d84b22576b9b35626702b8b
kj415j45 <919815238@qq.com>
2023-08-09 09:00:42 +0800

    Add stub database as control group in case needed.

---
+++ src/StubDatabase.ts
@@ -0,0 +1,53 @@
+import { TestData } from "./TestData";
+import { TestDatabase } from "./TestDatabase";
+
+export class StubDatabase extends TestDatabase {
+  name(): string {
+    return "Stub";
+  }
+
+  async connect(uri?: string | undefined): Promise<void> {}
+  async disconnect(): Promise<void> {}
+  async cleanup(): Promise<void> {}
+  async create(data: TestData[]): Promise<void> {}
+  async prepare(): Promise<void> {}
+  async usageReport(): Promise<object> {
+    return {};
+  }
+
+  async queryA(lng: number, lat: number): Promise<TestData> {
+    return {
+      id: "stub",
+      lng,
+      lat,
+    };
+  }
+
+  async queryB(
+    lng: number,
+    lat: number,
+    maxDistance: number
+  ): Promise<TestData[]> {
+    return [
+      {
+        id: "stub",
+        lng,
+        lat,
+      },
+    ];
+  }
+
+  async queryC(
+    lng: number,
+    lat: number,
+    maxDistance: number
+  ): Promise<TestData[]> {
+    return [
+      {
+        id: "stub",
+        lng,
+        lat,
+      },
+    ];
+  }
+}
