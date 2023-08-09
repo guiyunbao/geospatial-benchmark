15b2d138c7749fbab8310f68d4a57994ea1ab9a2
kj415j45 <919815238@qq.com>
2023-08-09 09:41:22 +0800

    Use light weight docker image.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 docker run -p 27017:27017 --name mongo -d mongodb/mongodb-community-server:latest
 
 # Redis
-docker run -p 6379:6379 --name redis -d redis/redis-stack:latest
+docker run -p 6379:6379 --name redis -d redis/redis-stack-server:latest
